486e2a0786a344301286693890e81c341dc919a8
DavidKal29 <davidkalmuk65@gmail.com>
2025-10-27 17:42:49 +0100

    Añadimos la ruta de forgot password, con la cual, habrá un formulario para enviar un correo al email de la cuenta, con el enlace de recuperacion de contraseña

--- src/app/forgotPassword/page.jsx
+++ src/app/forgotPassword/page.jsx
@@ -2,10 +2,9 @@
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
 
-export default function Home() {
+export default function ForgotPassword() {
   const [form,setForm] = useState({
     email:'',
-    password:''
   })
 
   const handleChange = (e)=>{
@@ -18,7 +17,7 @@ export default function Home() {
   const handleSubmit = (e)=>{
     e.preventDefault()
 
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`,{
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/password/forgotPassword`,{
       method:'POST',
       credentials:'include',
       headers:{'Content-Type':'application/json'},
@@ -34,19 +33,18 @@ export default function Home() {
       }
     })
     .catch(error=>{
-      console.log('Error al enviar los datos a Register');
+      console.log('Error al enviar los datos a Forgot Password');
       console.error(error);
       toast.error('Error al enviar los datos')  
     })
-   
+  
+  
   }
 
   useEffect(()=>{
-      document.title = 'Login'
-    },[])
-  
+    document.title = 'Forgot Password'
+  },[])
 
-  const [visiblePassword, setVisiblePassword] = useState(false)
 
   return (
     <div className="bg-black min-h-screen flex flex-col lg:flex-row justify-center items-center p-4 lg:p-12 gap-6 lg:gap-24">
@@ -57,13 +55,8 @@ export default function Home() {
       <div className="flex flex-col justify-center items-center gap-6 lg:bg-orange-500 lg:p-6 lg:rounded lg:gap-8">
         {/* Texto de Bienvenida */}
         <p className="text-center text-[15px] lg:text-[20px] text-orange-500 lg:text-white font-bold max-w-[20rem] sm:max-w-[24rem] md:max-w-[32rem]">
-          ¡Inicia sesión y disfruta de la mejor app Fast Food del Planeta!
+          ¡Si olvidaste la contraseña, utiliza el mejor sistema de recuperación del Planeta!
         </p>
-
-        {/* Invitado */}
-        <button className="text-center text-[15px] lg:text-[20px] font-bold text-white cursor-pointer">
-          <i className="fa-solid fa-user"></i> Acceder como Invitado
-        </button>
 
         {/* Formulario */}
         <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center gap-6">
@@ -80,38 +73,19 @@ export default function Home() {
                 className="flex-1 h-full outline-none"
               />
             </div>
+           </div>
 
-            {/* Password */}
-            <div className="bg-white rounded flex items-center w-[16rem] sm:w-[20rem] md:w-[24rem] h-[3rem] px-3">
-              <i className="fa-solid fa-lock text-[20px] mr-2"></i>
-              <input
-                type={visiblePassword ? 'text' : 'password'}
-                name="password"
-                onChange={handleChange}
-                autoComplete="off"
-                placeholder="Password"
-                className="flex-1 h-full outline-none"
-              />
-              <button type="button" onClick={() => setVisiblePassword(!visiblePassword)} className="cursor-pointer">
-                <i className={visiblePassword ? 'fa-solid fa-eye' : 'fa-solid fa-eye-slash'}></i>
-              </button>
-            </div>
-          </div>
-
-          {/* Olvido de contraseña */}
+          {/* Iniciar Sesión */}
           <p className="text-white text-center max-w-[16rem] sm:max-w-[20rem] md:max-w-[24rem]">
-            ¿Olvidaste la contraseña? <a href="/forgotPassword" className="text-orange-500 lg:text-yellow-200">Recuperar</a>
+            ¿Recordaste la contraseña? <a href="/" className="text-orange-500 lg:text-yellow-200">Iniciar Sesión</a>
           </p>
 
-          {/* Botón Iniciar Sesión */}
+          {/* Botón Enviar correo */}
           <button className="rounded text-center font-bold bg-orange-500 lg:bg-white text-white lg:text-orange-500 w-[16rem] sm:w-[20rem] md:w-[24rem] h-[3rem] cursor-pointer">
-            Iniciar Sesión
+            Enviar Correo
           </button>
 
-          {/* Crear Cuenta */}
-          <p className="text-white text-center max-w-[16rem] sm:max-w-[20rem] md:max-w-[24rem]">
-            ¿No tienes cuenta? <a href="/register" className="text-orange-500 lg:text-yellow-200">Crear Cuenta</a>
-          </p>
+          
         </form>
       </div>
 
