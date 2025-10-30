904926668e6fa78ba4fede2bd15b89a50d753848
DavidKal29 <davidkalmuk65@gmail.com>
2025-10-30 13:48:48 +0100

    Retocamos la altura del menú para que no ocupe tanto espacio

--- src/app/components/HomeLink.jsx
+++ src/app/components/HomeLink.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function HomeLink() {
   return (
-    <a href='/home' className='w-[4rem] md:w-[8rem]'>
+    <a href='/home' className='w-[4rem] md:w-[6rem]'>
         <img src="/images/logos/logo.png" alt="Logo Fast&Crunchy" />
     </a>
   )
